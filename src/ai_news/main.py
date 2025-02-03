0e5fc7c06ceb6fc25b2e527691e4963c7c17be00
shivank-1 <shivankdevliyal1@gmail.com>
2025-02-03 15:03:23 +0530

    updated

--- src/ai_news/main.py
+++ src/ai_news/main.py
@@ -15,7 +15,7 @@ def run():
     """
     Run the crew.
     """
-    inputs = {
+    inputs = {                              ##here we can also define an array of different inputs (different topics). for that we have to make a list and put such dictionaries with different inputs in them
         'topic': 'openai agentic ai',
         'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     }
