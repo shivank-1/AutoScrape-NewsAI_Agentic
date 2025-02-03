6eda3c3f1308f0cab781428e1ceda376c52e95fb
shivank-1 <shivankdevliyal1@gmail.com>
2025-02-03 15:00:21 +0530

    Updated

--- src/ai_news/main.py
+++ src/ai_news/main.py
@@ -16,7 +16,7 @@ def run():
     Run the crew.
     """
     inputs = {
-        'topic': 'openai',
+        'topic': 'openai agentic ai',
         'date': datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     }
 
