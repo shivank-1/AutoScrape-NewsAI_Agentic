fbfc897ec5cc1a4b02355b9c5403558e6a778d54
shivank-1 <shivankdevliyal1@gmail.com>
2025-02-04 23:41:22 +0530

    updated

--- src/ai_news/main.py
+++ src/ai_news/main.py
@@ -6,10 +6,7 @@ from crew import AiNews
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
+
 
 def run():
     """
